409bd3bc100d5f9d1233e6f4815e62328b718d64
justinburrill <me@justinburrill.ca>
2022-06-13 12:51:18 -0400

    Update cnc.ino

--- cnc/cnc.ino
+++ cnc/cnc.ino
@@ -1,7 +1,20 @@
+//int paperWidth;
+//int paperHeight;
+int imgWidth;
+int imgHeight;
+
 void setup() {
 
 }
 
 void loop() {
     
+}
+
+void drawpixel(int pixelsize) {
+  
+}
+
+void move_to(int xin, int yin) {
+  
 }
